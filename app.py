91997f1881db204826b29dd883a52b7035039e41
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2023-05-22 16:42:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,92 +1,61 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
-import urllib.request
 from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.models import load_model
-import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+import requests
+from io import BytesIO
 
-# Define the input features and their types
-features = [
-    ("WBCs on Admission", float),
-    ("Age", int),
-    ("BMI", float),
-    ("Nimodipin", int),
-    ("HH Score", int),
-    ("mFisher Score", int),
-    ("Hypertension", int),
-    ("Diabetes", int),
-    ("Hypercholestorelemia", int),
-    ("Congestive Heart Failure", int),
-    ("Cancer", int),
-    ("Smoking", int),
-    ("Cocaine", int),
-    ("Location", int),
-    ("Size", float),
-    ("Side", int),
-    ("Treatment Modality", int),
-    ("EVD", int),
-    ("VP Shunt", int),
-    ("TCD Vasospasm", int),
-    ("Angiographic Vasospasm", int),
-    ("Clinical Vasospasm", int),
-    ("WBCs", float),
-    ("Neutrophils", float),
-    ("Lymphocytes", float),
-    ("Albumin", float),
-    ("MCV", float),
-    ("Platelets", float),
-    ("Red Cell Distribution Width", float),
-    ("Monocytes", float),
-    ("BUN", float),
-    ("Creatinine", float),
-    ("INR", float),
-    ("PTT", float)
-]
+# Load the dataset from a CSV file
+data_url = "https://github.com/drazzam/DCI_Predictor/raw/main/data.csv"
+data_response = requests.get(data_url)
+data = pd.read_csv(BytesIO(data_response.content))
 
-st.title("Delayed Cerebral Ischemia Prediction")
+# Preprocess the data
+# Replace this with your own preprocessing steps based on the dataset
+X = data.drop('dci', axis=1)
+y = data['dci']
 
-# Create a dictionary to store user inputs
-user_input = {}
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Scale the data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Define and compile the MLP model
+mlp = Sequential()
+mlp.add(Dense(64, input_dim=X_train_scaled.shape[1], activation='relu'))
+mlp.add(Dense(32, activation='relu'))
+mlp.add(Dense(1, activation='sigmoid'))
+mlp.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
+
+# Train the model
+mlp.fit(X_train_scaled, y_train, epochs=50, batch_size=32, verbose=0)
 
 # Create input fields for each feature
-for feature, feature_type in features:
-    user_input[feature] = st.number_input(f"Enter {feature}:", value=feature_type())
+user_input = {}
+for column in X.columns:
+    if X[column].dtype == float:
+        user_input[column] = st.number_input(f"Enter {column}:", value=float())
+    elif X[column].dtype == int:
+        user_input[column] = st.number_input(f"Enter {column}:", value=int())
 
 # Prepare the input data as a dataframe
 input_df = pd.DataFrame([user_input])
 
 # Preprocess the input data
 # Note: Make sure to preprocess the input data in the same way as the training data
-categorical_features = [
-    "Nimodipine", "HH Score", "mFisher Score", "Hypertension", "Diabetes",
-    "Hypercholestorelemia", "Congestive Heart Failure", "Cancer", "Smoking",
-    "Alcohol", "Cocaine", "Location of Aneurysm", "Size", "Side",
-    "Treatment Modality", "EVD", "VP Shunt", "TCD Vasospasm",
-    "Angiographic Vasospasm", "Clinical Vasospasm"
-]
-
-input_df = pd.get_dummies(input_df, columns=categorical_features)
-
-@st.experimental_singleton
-def load_trained_model_and_scaler():
-    if not os.path.isfile('trained_model.h5'):
-        urllib.request.urlretrieve('https://github.com/drazzam/DCI_Predictor/raw/main/trained_model.h5', 'trained_model.h5')
-    if not os.path.isfile('scaler.pkl'):
-        urllib.request.urlretrieve('https://github.com/drazzam/DCI_Predictor/raw/main/scaler.pkl', 'scaler.pkl')
-
-    mlp = load_model('trained_model.h5')
-    scaler = joblib.load('scaler.pkl')
-    return mlp, scaler
-
-mlp, scaler = load_trained_model_and_scaler()
-
-X = scaler.transform(input_df)
+input_scaled = scaler.transform(input_df)
 
 # Make the prediction
-prediction = mlp.predict_classes(X)
-confidence = mlp.predict_proba(X) * 100
+prediction = mlp.predict_classes(input_scaled)
+confidence = mlp.predict_proba(input_scaled) * 100
 
 # Display the result
 if prediction[0] == 0:
